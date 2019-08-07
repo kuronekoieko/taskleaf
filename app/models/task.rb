9ca17901e106460f7f56a8ad411b6a96c6071e4d
ieko <mendo931411.mou@gmail.com>
2019-08-07 17:12:13 +0900

    モデルのコールバックを実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 class Task < ApplicationRecord
+  before_validation :set_nameless_name
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
   validates :validates_name_not_including_comma
@@ -11,5 +12,10 @@ class Task < ApplicationRecord
   def validates_name_not_including_comma
     #検証エラーを確認したら、errorsに格納する
     errors.add(:name, "にカンマを含めることは出来ません") if name&.include?(",")
+  end
+
+  #コールバックの練習用、nilまたは空文字が入ってきたときに代入する
+  def set_nameless_name
+    # self.name = "名前無し" if name.blank?
   end
 end
