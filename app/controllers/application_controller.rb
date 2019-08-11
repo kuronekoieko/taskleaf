940c09de8c855fe8ccdc9db2fa924cd9724f7c6d
ieko <mendo931411.mou@gmail.com>
2019-08-11 22:46:21 +0900

    ログイン機能を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  private
+
+  def current_user
+    #||= nilガード：その値が存在すればそれを返し、nilの時には指定した値を返す
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,27 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  # セッションを作る
+  def create
+    user = User.find_by(email: session_params[:email])
+    # ユーザーが見つかった場合はauthenticateメソッドでパスワード認証をする
+    # authenticateメソッド:Userクラスにhas_secure_passswordと記述した時に
+    # 自動で追加された認証のためのメソッド
+    # 引数で受け取ったパスワードをハッシュ化して、
+    # その結果がUserオブジェクト内部に保存されているdigestと一致するかどうかを調べる
+    # 一致していたらUserオブジェクトを、一致していなければfalseを返す
+    if user&.authenticate(session_params[:password])
+      settion[:user_id] = user.def index
+      redirect_to root_path, notice: "ログインしました。"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
 end
