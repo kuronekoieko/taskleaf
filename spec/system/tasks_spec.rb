c37657e3a2827511418d27037971a6b828cebabf
ieko <mendo931411.mou@gmail.com>
2019-08-12 20:44:39 +0900

    タスク詳細画面のテストを実装

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,34 +2,40 @@
 require "rails_helper"
 
 describe "タスク管理機能", type: :system do
-  describe "一覧表示機能" do
-    #letを使って処理を共通化する
-    let(:user_a) { FactoryBot.create(:user, name: "ユーザーA", email: "a@example.com") }
-    let(:user_b) { FactoryBot.create(:user, name: "ユーザーB", email: "b@example.com") }
-    before do
-      # ユーザーAを作成しておく
-      # :userファクトリを指定してUserオブジェクトを作成する
-      # FactoryBot.createでデータの生成、データベースに保存まdやってくれる
-      # FactoryBot.buildで生成だけできる
-      # user_a = FactoryBot.create(:user)
-      # 一部の属性を変更してデータを作ることもできる
-      # user_a = FactoryBot.create(:user, name: "ユーザーA", email: "a@example.com")
-      # 作成者がユーザーAであるタスクを作成しておく
-      FactoryBot.create(:task, name: "最初のタスク", user: user_a)
 
-      # ユーザーAでログインする
-      # 特定のURLでアクセスする、という動作はvisit [URL]という動作でできる
-      visit login_path
-      # 「メールアドレス」というラベル(<label>要素)がついた
-      # テキストフィールド(<input>)要素にメールアドレスを入れる
-      # fill_in "ラベル値", with "インプット値"
-      #fill_in "メールアドレス", with: "a@example.com"
-      fill_in "メールアドレス", with: login_user.email
-      # fill_in "パスワード", with: "password"
-      fill_in "パスワード", with: login_user.password
-      # 「ログインする」ボタンを押す
-      click_button "ログインする"
-    end
+  #letを使って処理を共通化する
+  #ここに書いただけでは呼ばれない
+  let(:user_a) { FactoryBot.create(:user, name: "ユーザーA", email: "a@example.com") }
+  let(:user_b) { FactoryBot.create(:user, name: "ユーザーB", email: "b@example.com") }
+  # !をつけると、beforeの前にtask_aが作られる
+  let!(:task_a) { FactoryBot.create(:task, name: "最初のタスク", user: user_a) }
+
+  before do
+    # ユーザーAを作成しておく
+    # :userファクトリを指定してUserオブジェクトを作成する
+    # FactoryBot.createでデータの生成、データベースに保存まdやってくれる
+    # FactoryBot.buildで生成だけできる
+    # user_a = FactoryBot.create(:user)
+    # 一部の属性を変更してデータを作ることもできる
+    # user_a = FactoryBot.create(:user, name: "ユーザーA", email: "a@example.com")
+    # 作成者がユーザーAであるタスクを作成しておく
+    FactoryBot.create(:task, name: "最初のタスク", user: user_a)
+
+    # ユーザーAでログインする
+    # 特定のURLでアクセスする、という動作はvisit [URL]という動作でできる
+    visit login_path
+    # 「メールアドレス」というラベル(<label>要素)がついた
+    # テキストフィールド(<input>)要素にメールアドレスを入れる
+    # fill_in "ラベル値", with "インプット値"
+    #fill_in "メールアドレス", with: "a@example.com"
+    fill_in "メールアドレス", with: login_user.email
+    # fill_in "パスワード", with: "password"
+    fill_in "パスワード", with: login_user.password
+    # 「ログインする」ボタンを押す
+    click_button "ログインする"
+  end
+
+  describe "一覧表示機能" do
     context "ユーザーAがログインしているとき" do
       # contextの中で定義すると、その内側でも外側でも変数のように呼び出して使える
       # ここでは上の方で定義したuser_aというletを呼び出してlogin_userとして定義している
@@ -40,6 +46,7 @@ describe "タスク管理機能", type: :system do
         expect(page).to have_content "最初のタスク"
       end
     end
+
     context "ユーザーBがログインしているとき" do
       let(:login_user) { user_b }
       before do
@@ -54,6 +61,20 @@ describe "タスク管理機能", type: :system do
       it "ユーザーAが作成したタスクが表示されない" do
         # ユーザーAが作成したタスクの名称が画面上に表示されていないことを確認
         expect(page).to have_no_content "最初のタスク"
+      end
+    end
+  end
+
+  describe "詳細表示機能" do
+    context "ユーザーAがログインしているとき" do
+      let(:login_user) { user_a }
+
+      before do
+        visit task_path(task_a)
+      end
+
+      it "ユーザーAが作成したタスクが表示される" do
+        expect(page).to have_content "最初のタスク"
       end
     end
   end
